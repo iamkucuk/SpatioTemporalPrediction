9b7f5f2531fa9e081928619cb8a232edf96e7fa4
iamkucuk <furkankucuk@outlook.com>
2019-12-26 10:51:04 +0300

    improvements

--- evaluation_utils.py
+++ evaluation_utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 
 
@@ -20,7 +21,6 @@ def model_predict(model, dataloader, dataset_size, device=None):
 
     for i, (inputs, labels) in enumerate(dataloader):
         inputs = inputs.to(device, dtype=torch.float)
-        labels = labels.to(device, dtype=torch.float)
 
         with torch.set_grad_enabled(False):
             outputs = model(inputs)
@@ -28,3 +28,43 @@ def model_predict(model, dataloader, dataset_size, device=None):
             predictions.append(outputs)
 
     return outputs
+
+
+def evaluate_model_with_predictions(model, validation_loader, dataset_size, device=None):
+    if device is None:
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    model.eval()
+    confusion_matrix = np.zeros((2, 2))
+    for i, (inputs, labels) in enumerate(validation_loader):
+        inputs = inputs.to(device, dtype=torch.float)
+
+        with torch.set_grad_enabled(False):
+            outputs = model(inputs)
+            confusion_matrix += evaluate_predictions(labels, outputs.data.numpy())
+
+    return outputs
+
+
+def evaluate_probas(y_true: np.ndarray, y_proba: np.ndarray):
+    """
+
+    :param y_true:
+    :param y_proba:
+    :return:
+    """
+    pass
+
+
+def evaluate_predictions(y_true: np.ndarray, y_pred: np.ndarray):
+    """
+
+    :param y_true:
+    :param y_pred:
+    :return:
+    """
+    tp = np.sum(np.logical_and(y_pred == 1, y_true == 1))
+    fp = np.sum(np.logical_and(y_pred == 1, y_true == 0))
+    tn = np.sum(np.logical_and(y_pred == 0, y_true == 0))
+    fn = np.sum(np.logical_and(y_pred == 0, y_true == 1))
+    return np.matrix[[tp, fp], [fn, tn]]
--- utils.py
+++ utils.py
@@ -1,9 +1,11 @@
 import copy
 
 import numpy as np
+import torch
 from torch import nn, optim
 from torch.utils.data import DataLoader
 
+from evaluation_utils import evaluate_model_with_predictions
 from train_model import train_model
 from ConvNetDataset import ConvNetDataset
 
@@ -29,7 +31,7 @@ def cross_validation(model,
         optimizer = optim.Adam(model_copy.parameters())
 
         trained_model = train_model(model_copy, dataloaders, dataset_sizes, criterion, optimizer, num_epochs=10)
-        model_scores.append(evaluate_predictions(trained_model(validation), validation))
+        model_scores.append(evaluate_model_with_predictions(trained_model, dataloaders["val"], dataset_sizes["val"]))
 
     return np.mean(model_scores)
 
@@ -46,15 +48,5 @@ def train_test_split(dataset: np.ndarray, train_set_ratio=.85) -> (np.ndarray, n
     return dataset[:len(dataset) * train_set_ratio], dataset[len(dataset) * train_set_ratio:]
 
 
-def evaluate_probas(y_true: np.ndarray, y_proba: np.ndarray):
-    pass
-
-
-def evaluate_predictions(y_true: np.ndarray, y_pred: np.ndarray):
-    tp = np.sum(np.logical_and(y_pred == 1, y_true == 1))
-    fp = np.sum(np.logical_and(y_pred == 1, y_true == 0))
-    tn = np.sum(np.logical_and(y_pred == 0, y_true == 0))
-    fn = np.sum(np.logical_and(y_pred == 0, y_true == 1))
-    return np.matrix[[tp, fp], [fn, tn]]
 
 # model = ConvNet()
